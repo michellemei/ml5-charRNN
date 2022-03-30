f45f9e0ef402558752b4bd5050b7d86f0b6a49b9
Michelle Mei <meim@emmanuel.edu>
2022-03-29 22:59:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
-function setup () {
-  // put setup code here
+// Create the character level generator with a pre trained model
+const rnn = ml5.charRNN('models/asyoulikeit/', modelLoaded);
+
+// When the model is loaded
+function modelLoaded() {
+  console.log('Model Loaded!');
 }
 
-function draw () {
-  // put drawing code here
-}
+// Generate content
+rnn.generate({ seed: 'the meaning of pizza is' }, (err, results) => {
+  console.log(results);
+});
